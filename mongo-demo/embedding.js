05cf5c7909c1ebf373ea71257bb93a1985b1642d
muhwezi deo <aggrey256@gmail.com>
2019-03-12 09:43:00 +0300

    array of items to add

--- mongo-demo/embedding.js
+++ mongo-demo/embedding.js
@@ -15,16 +15,18 @@ const Author = mongoose.model('Author', authorSchema);
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
   // validate
-  author:{
-    type:authorSchema,
-    required:true
-  }
+  // author:{
+  //   type:authorSchema,
+  //   required:true
+  // }
+  // array of authors
+  authors:[authorSchema]
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
+    authors
   }); 
   
   const result = await course.save();
@@ -56,6 +58,32 @@ async function updateCourse(courseId){
 
 }
 
-updateCourse('5c874e11f97a7949dd84e1b8')
-// listCourses();
-// createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function addAuthor (courseId,author) {
+  const course = await Course.findById(courseId);
+  console.log(course);
+  course.authors.push(author);
+  const result = await course.save()
+  console.log(result);
+
+}
+async function removeAuthor (courseId,authorId) {
+  const course = await Course.findById(courseId);
+  console.log(course.authors);
+
+  const author = await course.authors.id(authorId);
+  author.remove()
+  const result= await course.save()
+  console.log(result);
+
+}
+
+// addAuthor('5c874e11f97a7949dd84e1b8', new Author({
+//   name:"another one"
+// }))
+
+removeAuthor('5c875100e25dd755ddb78235','5c875100e25dd755ddb78233' )
+
+// updateCourse('5c874e11f97a7949dd84e1b8')
+// // listCourses();
+// createCourse('Node Course', [new Author({ name: 'Mosh' }),
+//   new Author({ name: 'Mosh2' })]);
